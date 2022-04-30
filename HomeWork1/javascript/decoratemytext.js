572a9ad42dc062b9d484fd96d986a22077b468ce
ashish-pokhrel <ashishpokhrel45@gmail.com>
2022-04-30 11:58:00 -0500

    Text Decoration Feature added

--- HomeWork1/javascript/decoratemytext.js
+++ HomeWork1/javascript/decoratemytext.js
@@ -1,11 +1,13 @@
 
 window.onload = function () {
 
-    var onBigDecoration = document.getElementById("bigDecoration");
-    var onBling = document.getElementById("bling");
-    var textArea = document.getElementById("text");
-    var bodySection = document.getElementById("bodySection");
-    let url = "./images/background.gif"
+    const onBigDecoration = document.getElementById("bigDecoration");
+    const onBling = document.getElementById("bling");
+    const textArea = document.getElementById("text");
+    const bodySection = document.getElementById("bodySection");
+    const atinlayButton = document.getElementById("atinlay");
+    const malkovitchButton = document.getElementById("malkovitch");
+    const url = "./images/background.gif"
 
     function onPageLoad() {
         alert("Hello, world!");
@@ -35,4 +37,31 @@ window.onload = function () {
             bodySection.style.backgroundImage = "none";
         }
     };
+
+    atinlayButton.onclick = function onBigDecorationClick() {
+        const vowels = ['a', 'e', 'i', 'o', 'u'];
+        let value = textArea.value;
+        if (value.length > 0) {
+            let slicedValue = value.slice(1);
+            if (isVowel(value.charAt(0).toLowerCase())) {
+                textArea.value = (slicedValue + "-" + "ay" + value.charAt(0));
+            }
+            else {
+                textArea.value = (slicedValue + "-" + value.charAt(0) + "ay");
+            }
+        }
+    }
+
+
+    malkovitchButton.onclick = function onBigDecorationClick() {
+        let value = textArea.value;
+        if (value.length >= 5) {
+            let malkovichString = "Malkovich" + value.slice(5);
+            textArea.value = malkovichString;
+        }
+    }
+
+    function isVowel(char) {
+        return char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u' || false;
+    }
 }
